f9919fa1c41b97b959e404a9c3473d572b80cd9d
minhtq-local <minhtq@minhtqs-Mac.local>
2023-10-22 14:41:20 -0400

    make changes to the front page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React from 'react'
-import { Navbar, Container } from 'react-bootstrap';
-import './App.css';
-import logo from './images/logo.png';
+import NavBar from './NavBar.js';
 
 function App() {
   return (
-    <Navbar bg="dark" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">
-          <img
-            alt=""
-            src={logo}
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />{' '}
-          TaskBulls</Navbar.Brand>
-      </Container>
-    </Navbar>
+    <NavBar></NavBar>
   );
 }
 
